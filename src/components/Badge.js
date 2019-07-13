b67f9ef8a73ba1c6c5c9aa5834e92e415d33b2ba
Diego Mendez <diegomarmcfly@gmail.com>
2019-07-13 03:58:17 -0600

    Adding badges props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,35 +1,31 @@
 import React from 'react';
 
-import './styles/Badge.css';
+import "./styles/Badge.css";
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
-  render() {
+  render(){
     return (
       <div className="Badge">
         <div className="Badge__header">
-          <img src={confLogo} alt="Logo de la conferencia" />
+          <img src={confLogo} alt="Conference's logo" />
         </div>
 
         <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src={this.props.avatarUrl}
-            alt="Avatar"
-          />
-          <h1>
-            {this.props.firstName} <br /> {this.props.lastName}
-          </h1>
+          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+          <h1 >{this.props.firstName} <br /> {this.props.lastName}</h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>{this.props.jobTitle}</h3>
+          <h3>{this.props.position}</h3>
           <div>@{this.props.twitter}</div>
         </div>
 
-        <div className="Badge__footer">#platziconf</div>
+        <div className="Badge__footer">
+          #PlatziConf
+        </div>  
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
-import './global.css';
-import App from './components/App';
+import Badge from './components/Badge';
+import './global.css'
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<Badge 
+  firstName='Diego' 
+  lastName='Mendez' 
+  position='DevOps Engineer'
+  twitter='diegomarmcfly'
+  avatar='https://www.gravatar.com/avatar?d=identicon'/>, 
+  container);
+
+
