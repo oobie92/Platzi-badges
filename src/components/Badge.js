9db94bf203c2f12a0f1dee8785738409072b04e2
Diego Mendez <diegomarmcfly@gmail.com>
2019-07-13 04:01:26 -0600

    badges minor change on props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -13,7 +13,7 @@ class Badge extends React.Component {
 
         <div className="Badge__section-name">
           <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
-          <h1 >{this.props.firstName} <br /> {this.props.lastName}</h1>
+          <h1 >{this.props.firstName}<br /> {this.props.lastName}</h1>
         </div>
 
         <div className="Badge__section-info">
